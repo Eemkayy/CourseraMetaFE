fc6ae2175710a8c02661abb4188bf636bf8f9d0a
Eemkayy <maksims.kkes@gmail.com>
2023-05-17 10:49:39 -0500

    Started Week 2, Overview of basic JS

---
+++ Course_2/Week 1/loops.js
@@ -0,0 +1,22 @@
+//For loops
+for (var i=1;i<=3;i++){
+    console.log(i);
+}
+
+console.log('Go');
+
+
+for (var i=10;i>=0;i--){
+    console.log(i);
+}
+console.log('Boom');
+
+
+//While loop
+
+var counter = 3;
+while(counter>0){
+    console.log(counter);
+    counter--;
+}
+console.log("Happy New Year");
---
+++ Course_2/Week 1/var.js
@@ -0,0 +1,9 @@
+var person = "John";
+console.log("Hello", person);
+
+var greetings = "Hello";
+console.log(greetings, person);
+
+greetings='hi';
+person='James';
+console.log(greetings,person);
