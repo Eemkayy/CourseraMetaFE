72a7e8f0809c51979d59ddd7190a083088da2f84
Eemkayy <maksims.kkes@gmail.com>
2023-05-30 16:17:36 -0500

    First block of the week tackled

---
+++ Course_5 - React Basics/Week 1/React Components and where they live/secondapp/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from './Heading';
+
+function App() {
+  return (
+
+    
+
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div className="App">
+      
+          <Heading />
+          </div>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Course_5 - React Basics/Week 1/React Components and where they live/secondapp/src/Heading.js
@@ -0,0 +1,7 @@
+function Heading() {
+    return (
+      <h1>mmm borgir</h1>
+    )
+  }
+
+  export default Heading
